5047c2105ea945077c26ee501286d689f1152e80
chaim cohen <39926105+chaim1@users.noreply.github.com>
2018-12-17 19:53:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var socket = require('socket.io');
 
 //app setup
 var app = express();
-var server = app.listen(2000,function(){
+var server = app.listen(4000,function(){
     console.log('listening to requests on port 4000 ');
     
 });
